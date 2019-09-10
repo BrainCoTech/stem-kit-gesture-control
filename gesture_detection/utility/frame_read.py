4fe4f30d21ae6054a76b071ae31668e4bcb16b3f
Guangxin <carrot09hoo@gmail.com>
2019-09-10 15:45:00 -0400

    add mxnet model

--- gesture_detection/utility/frame_read.py
+++ gesture_detection/utility/frame_read.py
@@ -1,22 +1,32 @@
 import cv2
 import imutils
 
+# parameters
+_roi_size = 240
+
 # read frame from camera, return back frame and a segmented region where to put hands in 
-def read_frame(camera, top, right, bottom, left):
+def read_frame(camera):
+# def read_frame(camera):
     # get the current frame
-    (grabbed, frame) = camera.read()
+    (grabbed, frame) = camera.read()  # shape (720,1280)
     # resize the frame
-    frame = imutils.resize(frame, width = 700)
+    frame = imutils.resize(frame, width=800)
     # flip the frame so that it is not the mirror view
     frame = cv2.flip(frame, 1)
+    # frame size
+    h = frame.shape[0]
+    w = frame.shape[1]
+    # get the region of interest (ROI)
+    top = 20
+    bottom = int(top + _roi_size)
+    right = int(w/2 - _roi_size/2)
+    left = int(right + _roi_size)
     # clone the frame
     clone = frame.copy()
-    # get the height and width of the frame
-    (height, width) = frame.shape[:2]
     # get the region of interest (ROI)
     roi = frame[top:bottom, right:left]
     # draw a region to segment hand
     cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
     # display the frame 
     cv2.imshow("Video Feed", clone)
-    return clone, roi
+    return roi
---
+++ gesture_detection/utility/neural_network.py
@@ -0,0 +1,110 @@
+import mxnet as mx
+from mxnet.gluon import nn, HybridBlock
+import numpy as np
+from imgaug import augmenters as iaa
+
+random_times =  1
+epoch = 100
+learning_rate = 0.0001
+
+# Net
+class inception_network(HybridBlock):
+    def __init__(self, classes=3, **kwargs):
+        super(inception_network, self).__init__(**kwargs)
+        with self.name_scope():
+            self.features = nn.HybridSequential(prefix='')
+            self.features.add(nn.Conv2D(use_bias=True, channels = 4, 
+                                        kernel_size = (5,5), strides = (1,1), 
+                                        padding = (1,1), activation='relu'))
+            self.features.add(nn.AvgPool2D(pool_size=3, strides=3, padding=1))
+            
+            self.features.add(nn.Conv2D(use_bias=True, channels = 8, 
+                                        kernel_size = (3,3), strides = (1,1), 
+                                        padding = (1,1), activation='relu'))
+            self.features.add(nn.AvgPool2D(pool_size=3, strides=3, padding=1))
+            
+            self.features.add(nn.Conv2D(use_bias=True, channels = 16, 
+                                        kernel_size = (3,3), strides = (1,1), 
+                                        padding = (1,1), activation='relu'))
+            self.features.add(nn.AvgPool2D(pool_size=3, strides=3, padding=1))
+            
+            # self.features.add(_make_output(hidden_units=[]))
+            self.output = nn.Dense(classes)
+
+    def hybrid_forward(self, F, x):
+        feature_vec = self.features(x)
+        x = self.output(feature_vec)
+        return x
+
+def augmentation(img):
+    seq = iaa.Sequential([
+        iaa.Crop(px=(10, 50)), # crop images from each side by 0 to 16px (randomly chosen)
+    ])
+    aug_img = seq.augment_image(img)
+    return aug_img
+
+def generate_data(training_data, label):
+    x_train, y_train = [], []
+    for i in range(len(label)):
+        y = np.zeros((1, len(label)))
+        y[0,i] = 1
+        cur_label = label[i]
+        cur_x = np.array(training_data[cur_label])
+        h,w = cur_x[0].shape
+        for j in range(cur_x.shape[0]):
+            x_train.append(cur_x[j].reshape(1,h,w))  
+            y_train.append(y)
+
+            for _ in range(random_times):
+                aug_img = augmentation(cur_x[j])
+                x_train.append(aug_img.reshape(1,h,w))
+                y_train.append(y)
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    y = y_train.reshape((y_train.shape[0], len(label)))
+    train = mx.gluon.data.ArrayDataset(x_train.astype('float32'), y.astype('float32'))
+    train_data = mx.gluon.data.DataLoader(train, batch_size=1, last_batch='discard', shuffle=True)
+    return train_data
+
+def net_summary():
+    net = inception_network()
+    ctx = mx.gpu(0) if mx.context.num_gpus() > 0 else mx.cpu(0)
+    net.initialize(mx.init.Xavier(), ctx=ctx)
+    data_input = mx.nd.array(np.random.randint(low=0, high=20, size=(1,1,128,128)))
+    net.summary(data_input)
+
+def train_model(train_data):
+    net = inception_network()
+    net.hybridize()
+    ctx = mx.gpu(0) if mx.context.num_gpus() > 0 else mx.cpu(0)
+    net.initialize(mx.init.Xavier(), ctx=ctx)
+    trainer = mx.gluon.Trainer(
+        params=net.collect_params(),
+        optimizer='Adam',
+        optimizer_params={'learning_rate': learning_rate},
+    )
+    loss_function = mx.gluon.loss.SoftmaxCrossEntropyLoss(sparse_label=False)
+    for e in range(epoch):
+        n = 0
+        epoch_loss = 0
+        # training model
+        for i, (x,y) in enumerate(train_data):
+            x = x.as_in_context(ctx)
+            y = y.as_in_context(ctx)
+            with mx.autograd.record():           
+                output = net(x)
+                l = loss_function(output, y)
+            l.backward()            
+            trainer.step(1)
+            epoch_loss += l.mean()
+        # train accuracy
+        for x,y in train_data:
+            output=net(x)
+            if np.argmax(output, axis=1) ==  np.argmax(y, axis=1):
+                n = n + 1
+            acc = np.around(n/len(train_data), decimals=4)
+    return net, acc
+
+
+
