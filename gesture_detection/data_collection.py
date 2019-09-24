adfec40781886ce3e5c2025eff29918f8504744c
Christine Sun <jxsun95@bu.edu>
2019-09-24 14:35:50 -0400

    data_collection refactor

--- gesture_detection/data_collection.py
+++ gesture_detection/data_collection.py
@@ -6,52 +6,59 @@ from utility.image_processing import preprocess_for_cnn
 
 training_data = {}
 
-_IMAGE_COUNT_FOR_EACH_CLASS = 10
+_IMAGE_COUNT_FOR_EACH_CLASS = 30
 
-should_quit = False
+start_recording = False
 
 
 def handle_key_press():
-    global should_quit
-    
+    global start_recording
     keypress = cv2.waitKey(1) & 0xFF
-    if keypress == ord('q'):
-        should_quit = True
+    if keypress == ord('c'):
+        start_recording = True
+
 
 
 if __name__ == "__main__":
-    img_count = 0
-    gesture_data_label = Gesture.paper  # Start collecting data with paper gesture
 
+    img_count = 0
     camera = cv2.VideoCapture(0)
 
-    while not should_quit:
+    while img_count < _IMAGE_COUNT_FOR_EACH_CLASS*3:
 
         roi = read_frame(camera)
-        handle_key_press()
-
         img = preprocess_for_cnn(roi)
 
-        if img_count == 0:
-            input("Press any key to collect data for:" + gesture_data_label.name)
-            print('Collecting data for:' + gesture_data_label.name)
+        # determine the wanted gesture based on img_count
+        gesture_data_label = Gesture(img_count//_IMAGE_COUNT_FOR_EACH_CLASS)
+        cv2.putText(roi, gesture_data_label.name,(5,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
 
-        img_count += 1
-        
-        if img_count == _IMAGE_COUNT_FOR_EACH_CLASS:
-            if gesture_data_label == Gesture.scissor:  # Last gesture data set to collect
-                with open('training_data.pickle', 'wb') as handle:
-                    pickle.dump(training_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                print('Successfully Saving Data')
-                break
 
-            gesture_data_label = Gesture(gesture_data_label.value + 1)
-            img_count = 0
-        
-        else:
+        # time to change gesture
+        if img_count % _IMAGE_COUNT_FOR_EACH_CLASS == 0:
+            cv2.putText(roi, "press c to continue",(5,50), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1, cv2.LINE_AA)
+            start_recording = False
+
+        handle_key_press()
+
+        if start_recording:
+            # record gesture and update the img_count
+            print(gesture_data_label)
             training_data[gesture_data_label.name] = training_data.get(gesture_data_label.name, [])
             training_data[gesture_data_label.name].append(img)
-            print(img_count)
+            img_count += 1;
+
+
+        cv2.imshow("roi", roi)
+
+
+    # saving data
+    with open('training_data.pickle', 'wb') as handle:
+        pickle.dump(training_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print('Successfully Saving Data')
+
+
+
 
     camera.release()
     cv2.destroyAllWindows()
